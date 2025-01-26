29d157136cd88aff370c6a143053e2840e92ea2a
Agustiann <126557712+Agustiann@users.noreply.github.com>
2025-01-27 04:19:10 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CreateAdminSeeder::class,
+            CreateRuanganSeeder::class,
         ]);
 
         User::factory()->create([
